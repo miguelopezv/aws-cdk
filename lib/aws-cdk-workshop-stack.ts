f3e221e45574d69ee4972afa89637d45fec2d494
Miguel López <miguelopezv@gmail.com>
2026-03-09 18:51:47 -0500

    added proxy integration to handle HTTP request

--- lib/aws-cdk-workshop-stack.ts
+++ lib/aws-cdk-workshop-stack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from "aws-cdk-lib/core";
 import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
 
 export class AwsCdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -10,6 +11,10 @@ export class AwsCdkWorkshopStack extends Stack {
       runtime: Runtime.NODEJS_22_X,
       code: Code.fromAsset("lambda"),
       handler: "hello.handler",
+    });
+
+    const gateway = new LambdaRestApi(this, "Endpoint", {
+      handler: hello,
     });
   }
 }
