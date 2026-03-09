0db1e7076e43fbc3da2a4be7f8ba85b0240d4f0e
Miguel López <miguelopezv@gmail.com>
2026-03-09 18:38:49 -0500

    update main file to deploy lambda function

--- lib/aws-cdk-workshop-stack.ts
+++ lib/aws-cdk-workshop-stack.ts
@@ -1,19 +1,15 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib/core';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib/core";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
 
 export class AwsCdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'AwsCdkWorkshopQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const hello = new Function(this, "HelloHandler", {
+      runtime: Runtime.NODEJS_22_X,
+      code: Code.fromAsset("lambda"),
+      handler: "hello.handler",
     });
-
-    const topic = new sns.Topic(this, 'AwsCdkWorkshopTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
