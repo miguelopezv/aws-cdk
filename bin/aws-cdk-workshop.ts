9135d7f3a83f4f054ad001385dcec8a5d16f2711
Miguel López <miguelopezv@gmail.com>
2026-03-10 14:24:24 -0500

    new Pipeline stack

--- bin/aws-cdk-workshop.ts
+++ bin/aws-cdk-workshop.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib/core';
-import { AwsCdkWorkshopStack } from '../lib/aws-cdk-workshop-stack';
+import * as cdk from "aws-cdk-lib/core";
+import { AwsCdkWorkshopStack } from "../lib/aws-cdk-workshop-stack";
+import { WorkshopPipelineStack } from "../lib/pipeline-stack";
 
 const app = new cdk.App();
-new AwsCdkWorkshopStack(app, 'AwsCdkWorkshopStack');
+new AwsCdkWorkshopStack(app, "AwsCdkWorkshopStack");
+new WorkshopPipelineStack(app, "AwsCdkWorkshopPipelineStack");
