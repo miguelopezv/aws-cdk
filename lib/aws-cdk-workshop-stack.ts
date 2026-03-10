b75b4723282f95f7f233440fbf987516297936d0
Miguel López <miguelopezv@gmail.com>
2026-03-10 12:51:31 -0500

    use tableViewer package

--- lib/aws-cdk-workshop-stack.ts
+++ lib/aws-cdk-workshop-stack.ts
@@ -3,6 +3,7 @@ import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
 import { HitCounter } from "./hitcounter";
+import { TableViewer } from "cdk-dynamo-table-viewer";
 
 export class AwsCdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -20,6 +21,11 @@ export class AwsCdkWorkshopStack extends Stack {
 
     const gateway = new LambdaRestApi(this, "Endpoint", {
       handler: helloWithCounter.handler,
+    });
+
+    const tv = new TableViewer(this, "ViewHitCounter", {
+      title: "Hello HIts",
+      table: helloWithCounter.table,
     });
   }
 }
