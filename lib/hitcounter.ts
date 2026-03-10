65957d0c319a94ae2ef67cb3aa053f1dc12e62ed
Miguel López <miguelopezv@gmail.com>
2026-03-10 12:51:17 -0500

    allow to read table value from outside

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -8,11 +8,12 @@ export interface HitCounterProps {
 
 export class HitCounter extends Construct {
   public readonly handler: Function;
+  public readonly table: Table;
 
   constructor(scope: Construct, id: string, props: HitCounterProps) {
     super(scope, id);
 
-    const table = new Table(this, "Hits", {
+    this.table = new Table(this, "Hits", {
       partitionKey: { name: "path", type: AttributeType.STRING },
     });
 
@@ -22,11 +23,11 @@ export class HitCounter extends Construct {
       code: Code.fromAsset("lambda"),
       environment: {
         DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
-        HITS_TABLE_NAME: table.tableName,
+        HITS_TABLE_NAME: this.table.tableName,
       },
     });
 
-    table.grantReadWriteData(this.handler);
+    this.table.grantReadWriteData(this.handler);
     props.downstream.grantInvoke(this.handler);
   }
 }
